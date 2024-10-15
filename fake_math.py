4154389df7722bac8cca5fa13782f6286424cf4a
Rustem272 <um1980r99@gmail.com>
2024-10-15 10:23:30 +0500

    Add files via upload

---
+++ fake_math.py
@@ -0,0 +1,9 @@
+def divide(a,b):
+    if b == 0:
+       return "ошибка"
+    else:
+       return a/b
+    
+    
+    print(divide(69, 3))
+    print(divide(3, 0))
---
+++ true_math.py
@@ -0,0 +1,14 @@
+from math import inf 
+
+
+
+def divide(a1,b1):
+    if b1 == 0:
+        return inf
+    else:
+        return a1/b1
+    
+    
+    print(divide(49 ,7))
+    print(divide(15 ,0))
+    
